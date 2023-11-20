71cc0da774923dc5f38985d9c14b0bcd7dc5464f
AbuHuraira6 <abuhuraira@shnairtech.com>
2023-11-20 18:32:07 +0500

    slice-updation

--- src/Redux/slice/sparePart.js
+++ src/Redux/slice/sparePart.js
@@ -2,14 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const partDataSlice = createSlice({
   name: 'partData',
-  initialState: { partData:[] },
+  initialState: { partData:[],sellableParts:[] },
   reducers: {
     setPartData: (state, action) => {
       state.partData = action.payload;
     },
+    setSellableParts: (state, action) => {
+      state.sellableParts = action.payload;
+    },
   },
 });
 
-export const { setPartData } = partDataSlice.actions;
+export const { setPartData,setSellableParts } = partDataSlice.actions;
 
 export default partDataSlice.reducer;
